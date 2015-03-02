1b9a2d0d2a912829d12333c6a9b2ef498763512e
Rodik Hanukaev <rodik@chegg.com>
2015-03-02 16:40:14 +0200

    zoom and pan added

--- sets.js
+++ sets.js
@@ -2,9 +2,9 @@
  * Created by rodik on 3/2/15.
  */
 
-var canvasData = ctx.getImageData(0, 0, cwidth, cheight);
-
-function drawPixel (x, y, r, g, b, a) {
+var canvasData;
+var maxiterations = 50;
+function drawPixel(x, y, r, g, b, a) {
     var index = (x + y * cwidth) * 4;
 
     canvasData.data[index] = r;
@@ -23,10 +23,14 @@ function putpixel(x, y, c) {
 
     if (c < 10) {
         b = 100 + c * 10;
+    } else {
+        b = 200;
     }
 
     if (c > 9 && c < 30) {
         g = (c - 9) * 10
+    } else if (c > 29) {
+        g = 200;
     }
 
     if (c > 29) {
@@ -36,18 +40,22 @@ function putpixel(x, y, c) {
     drawPixel(x, y, r, g, b, 255);
 }
 
-var xcorner = 0;
-var ycorner = 0;
-var zoom = 4.0;
+var view = {x:0,y:0,zoom:4.0};
+function resetView() {
+    view = {x:0,y:0,zoom:4.0};
+}
 
 function runSet() {
-    var max = 50,
-        iteration = 0;
+    var iteration = 0;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvasData = ctx.getImageData(0, 0, cwidth, cheight);
+
     for (var row = 0; row < cheight; row++) {
         for (var col = 0; col < cwidth; col++) {
-            iteration = mandelbrot(row, col, max);
+            iteration = mandelbrot(row, col, maxiterations);
 
-            if (iteration < max) {
+            if (iteration < maxiterations) {
                 putpixel(col, row, iteration);
             }
         }
@@ -56,8 +64,8 @@ function runSet() {
 }
 
 function mandelbrot(row, col, max) {
-    var cx = xcorner + (col - cwidth / 2.0) * zoom / cwidth,
-        cy = ycorner + (row - cheight / 2.0) * zoom / cwidth;
+    var cx = view.x + (col - cwidth / 2.0) * view.zoom / cwidth,
+        cy = view.y + (row - cheight / 2.0) * view.zoom / cwidth;
 
     var x = 0,
         y = 0,
@@ -73,3 +81,42 @@ function mandelbrot(row, col, max) {
 }
 
 runSet();
+
+canvas.onclick = onclick;
+function onclick(evt) {
+    console.log('evt', evt);
+    var x = evt.x, y = evt.y;
+
+    view.zoom /= 2;
+    console.log('calc (x - cwidth / 2.0)', (x - cwidth / 2.0));
+    console.log('calc (x - cwidth / 2.0) * view.zoom', (x - cwidth / 2.0) * view.zoom);
+    console.log('calc (x - cwidth / 2.0) * view.zoom / cwidth', (x - cwidth / 2.0) * view.zoom / cwidth);
+    view.x += (x - cwidth / 2.0) * view.zoom / cwidth;
+    view.y += (y - cheight / 2.0) * view.zoom / cwidth;
+
+    runSet();
+}
+
+window.addEventListener('keydown', function(event) {
+    switch (event.keyCode) {
+        case 37: // Left
+            view.x -= 0.01 * view.zoom;
+            runSet();
+            break;
+
+        case 38: // Up
+            view.y -= 0.01 * view.zoom;
+            runSet();
+            break;
+
+        case 39: // Right
+            view.x += 0.01 * view.zoom;
+            runSet();
+            break;
+
+        case 40: // Down
+            view.y += 0.01 * view.zoom;
+            runSet();
+            break;
+    }
+}, false);
